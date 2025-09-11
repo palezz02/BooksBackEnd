40d65003a2f3d02134768c62aee57001f0b63db5
Falonne Bonsi <bfalonne823@gmail.com>
2025-09-11 09:27:55 +0200

    create review model

--- src/main/java/com/betacom/books/be/models/Review.java
+++ src/main/java/com/betacom/books/be/models/Review.java
@@ -1,7 +1,8 @@
 package com.betacom.books.be.models;
 
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,39 +24,39 @@ public class Review {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@ManyToOne
 	@JoinColumn(
 			name="book",
 			nullable = false
 			)
-	private Books book;
-	
+	private Book book;
+
 	@ManyToOne
 	@JoinColumn(
 			name="user",
 			nullable = false
 			)
 	private User user;
-	
+
    @Min(0)
    @Max(5)
 	@Column (name="rating",
 			nullable = false)
 	private int rating;
-  
+
 	@Column (name="title",
 			nullable = true)
 	private int title;
-	
+
 	@Column (name="body",
 			nullable = true)
 	private int body;
-	
+
 	@Column (name="created_at",
 			nullable = false)
-	private int createdAt;
-	
-	
-	
+	private LocalDateTime createdAt;
+
+
+
 }
