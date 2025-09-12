02e20ede24b50ef03f459d52bc2f344fdc8b9966
Simone2611 <simone26v@gmail.com>
2025-09-12 16:18:55 +0200

    added getById on interfaces (OrderItem,Publisher)

--- src/main/java/com/betacom/books/be/services/interfaces/IOrderItemServices.java
+++ src/main/java/com/betacom/books/be/services/interfaces/IOrderItemServices.java
@@ -1,11 +1,14 @@
 package com.betacom.books.be.services.interfaces;
 
+import java.util.List;
+
 import com.betacom.books.be.dto.OrderItemDTO;
 import com.betacom.books.be.exception.BooksException;
 import com.betacom.books.be.requests.OrderItemReq;
 
 public interface IOrderItemServices {
-	OrderItemDTO getAll(OrderItemReq req) throws BooksException;
+	List<OrderItemDTO> getAll() throws BooksException;
+	OrderItemDTO getById(Integer id) throws BooksException;
 	
 	void create(OrderItemReq req) throws BooksException;
 	void update(OrderItemReq req) throws BooksException;
--- src/main/java/com/betacom/books/be/services/interfaces/IPublisherServices.java
+++ src/main/java/com/betacom/books/be/services/interfaces/IPublisherServices.java
@@ -1,15 +1,20 @@
 package com.betacom.books.be.services.interfaces;
 
+import java.util.List;
+
+import com.betacom.books.be.dto.OrderItemDTO;
 import com.betacom.books.be.dto.PublisherDTO;
 import com.betacom.books.be.exception.BooksException;
-import com.betacom.books.be.requests.OrderItemReq;
+import com.betacom.books.be.requests.PublisherReq;
+
 
 public interface IPublisherServices {
-	PublisherDTO getAll(OrderItemReq req) throws BooksException;
+	List<PublisherDTO> getAll() throws BooksException;
+	PublisherDTO getById(Integer id) throws BooksException;
 	
-	void create(OrderItemReq req) throws BooksException;
-	void update(OrderItemReq req) throws BooksException;
-	void delete(OrderItemReq req) throws BooksException;
+	void create(PublisherReq req) throws BooksException;
+	void update(PublisherReq req) throws BooksException;
+	void delete(PublisherReq req) throws BooksException;
 	
 	
 }
