71196e36e71fec350bf3c9a3ca870ba5b603ed4e
DESKTOP-62RHKRO\Betacom <g.palezza@gmail.com>
2025-09-12 17:00:11 +0200

    ADDED DTO for Book/Category

--- src/main/java/com/betacom/books/be/dto/BookDTO.java
+++ src/main/java/com/betacom/books/be/dto/BookDTO.java
@@ -1,5 +1,26 @@
 package com.betacom.books.be.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BookDTO {
 
+	 private Integer id;
+	    private String isbn;
+	    private String title;
+	    private Integer pageCount;
+	    private String description;
+	    private String coverImage;
+	    private String languageCode;
+	    private LocalDate publicationDate;
+	    private String edition;
+	    private PublisherDTO publisher;
+	    private List<AuthorDTO> authors;
+	    private List<CategoryDTO> categories;
+	    private List<ReviewDTO> reviews;
 }
---
+++ src/main/java/com/betacom/books/be/dto/CategoryDTO.java
@@ -0,0 +1,12 @@
+package com.betacom.books.be.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+
+    private Integer id;
+    private String name;
+}
