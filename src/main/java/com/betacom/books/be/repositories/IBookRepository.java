80b13036057902ed53ae362c404af4c26d293746
DESKTOP-62RHKRO\Betacom <g.palezza@gmail.com>
2025-09-12 15:21:57 +0200

    ADDED repository for book/category

--- src/main/java/com/betacom/books/be/repositories/IBookRepository.java
+++ src/main/java/com/betacom/books/be/repositories/IBookRepository.java
@@ -1,5 +1,11 @@
 package com.betacom.books.be.repositories;
 
-public interface IBookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.betacom.books.be.models.Book;
+
+@Repository
+public interface IBookRepository extends JpaRepository<Book, Integer>{
 
 }
