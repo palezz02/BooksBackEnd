1e24cb9074422c2212871150e6599e21ebf9c0af
Simone2611 <simone26v@gmail.com>
2025-09-15 10:10:57 +0200

    Implemented Publisher and fixed Imports on UtilsPublisher

--- src/main/java/com/betacom/books/be/requests/PublisherReq.java
+++ src/main/java/com/betacom/books/be/requests/PublisherReq.java
@@ -1,18 +1,5 @@
 package com.betacom.books.be.requests;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import com.betacom.books.be.models.Book;
-import com.betacom.books.be.models.Inventory;
-import com.betacom.books.be.models.Order;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
